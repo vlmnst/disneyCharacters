955090f47eed483c5e11413d8efbda2c0c352f8d
crisvalhas <cristianrubiles@gmail.com>
2022-06-29 15:46:50 -0300

    last commit

--- src/components/db.jsx
+++ src/components/db.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { Text, FlatList, StyleSheet } from "react-native";
-import { View } from "react-native-web";
+import { View } from "react-native";
 import data from "../../data.json";
 import DbItem from "./dbitem";
 
 const Db = () => {
   return (
 
-    <FlatList
+    <FlatList 
+      numColumns={3}
+      contentContainerStyle={styles.list}
       data={data}
       renderItem={({ item: repo }) => (
-        <View style={{
-            flexDirection: "row"
-          }} >
+        <View >
         <DbItem  { ...repo} />
         </View>
+        
       )}
     />
   );
 };
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-    },
+    list:{
+      // overflow:"hidden",
+      borderRadius:12,
+      borderWidth:12,
+      borderColor: "#FF5733",
+      flexDirection:"column",
+      alignItems: "center",
+    }
   });
+
 export default Db;
--- src/components/dbitem.jsx
+++ src/components/dbitem.jsx
@@ -1,29 +1,36 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { Text,View, StyleSheet, Image ,Button, Touchable} from "react-native";
+import { TouchableHighlight } from "react-native";
 import StyledText from "./StyledText";
 
-const DbItem = (props) => { 
+const DbItem = (props) => {
     return (
-         <View key={props.id} style={styles.container} >
-          <StyledText bold blue> {props.name}</StyledText>     
-           <Image style={styles.image} source={{ uri: props.imageUrl}} />
+        <View key={props.id} style={styles.container} >
+            <StyledText bold blue> {props.name}</StyledText>
+            <Image style={styles.image} source={{ uri: props.imageUrl }} />
+            <TouchableHighlight ><Text>eliminar</Text></TouchableHighlight>
         </View>
     )
-       
+
 
 }
 
 const styles = StyleSheet.create({
     container: {
+        width:100,
         padding: 20,
         paddingBottom: 5,
         paddingTop: 5,
+        alignItems:"center",
     },
     image: {
         width: 48,
         height: 48,
         borderRadius: 4,
         overflow: "hidden"
+    },
+    btn:{
+        height:200,
     }
 })
 export default DbItem
