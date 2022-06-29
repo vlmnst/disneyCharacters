48625b23a9dc294ff134a0f3e7bc63a19c947fa0
vlmnst <vale-mansueto@hotmail.com>
2022-06-28 21:39:58 -0300

    Subiendo Componentes

---
+++ src/components/db.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, FlatList, StyleSheet } from "react-native";
+import { View } from "react-native-web";
+import data from "../../data.json";
+import DbItem from "./dbitem";
+
+const Db = () => {
+  return (
+
+    <FlatList
+      data={data}
+      renderItem={({ item: repo }) => (
+        <View style={{
+            flexDirection: "row"
+          }} >
+        <DbItem  { ...repo} />
+        </View>
+      )}
+    />
+  );
+};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+  });
+export default Db;
